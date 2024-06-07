adf5cd0e32321d0ce9d66489eb2c9241fbcfe467
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-06-07 14:41:25 +0200

    st.data_editor(df, num_rows="dynamic")

--- pages/image_click.py
+++ pages/image_click.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from streamlit_cropperjs import st_cropperjs
 from streamlit_image_coordinates import streamlit_image_coordinates
@@ -19,15 +18,10 @@ import io
 
 st.set_page_config(layout="wide")
 
-
-#Testing
-
-
-
+# Testing
 
 
 from streamlit_image_coordinates import streamlit_image_coordinates
-
 
 try:
     # Check if the image is in session state
@@ -44,30 +38,25 @@ try:
 
     scale = float(scale_str)
 
-    #st.write("My favorite color is", scale)
+    # st.write("My favorite color is", scale)
     if image_with_boxes is not None:
-
         width, height = image_with_boxes.size
-        value = streamlit_image_coordinates(image_with_boxes, width = (width/scale))
+        value = streamlit_image_coordinates(image_with_boxes, width=(width / scale))
 
 
-
-    
-        
-
-    def point_in_box(x,y,box,scale):
-        p0,p1,p2,p3 = box
-        min_x = min(p0[0], p1[0], p2[0], p3[0])/scale
-        max_x = max(p0[0], p1[0], p2[0], p3[0])/scale
-        min_y = min(p0[1], p1[1], p2[1], p3[1])/scale
-        max_y = max(p0[1], p1[1], p2[1], p3[1])/scale
+    def point_in_box(x, y, box, scale):
+        p0, p1, p2, p3 = box
+        min_x = min(p0[0], p1[0], p2[0], p3[0]) / scale
+        max_x = max(p0[0], p1[0], p2[0], p3[0]) / scale
+        min_y = min(p0[1], p1[1], p2[1], p3[1]) / scale
+        max_y = max(p0[1], p1[1], p2[1], p3[1]) / scale
         return min_x <= x <= max_x and min_y <= y <= max_y
 
-    #value = None   
-    if value:
-        #st.write(value)
-        x,y = value["x"], value["y"]
 
+    # value = None
+    if value:
+        # st.write(value)
+        x, y = value["x"], value["y"]
 
     dimension = []
     if "bounds" in st.session_state:
@@ -80,37 +69,35 @@ try:
         if "dimension" not in st.session_state:
             st.session_state.dimension = []
 
-        
-
-        for bound in bounds :
+        for bound in bounds:
             box = [bound[0][0], bound[0][1], bound[0][2], bound[0][3]]
             if value is not None:
-                if point_in_box(x,y,box,scale):
+                if point_in_box(x, y, box, scale):
 
                     text = bound[1]
                     if len(st.session_state.temp) < 3:
                         st.session_state.temp.append(text)
-                
+
                 # When we have two dimensions, add the pair to the main list
                 if len(st.session_state.temp) == 2:
                     st.session_state.dimension.append(st.session_state.temp[:])
                     st.session_state.temp = []
 
-
                     st.write('You have selected the text    : ', text)
-        
-                  
+
+
         else:
             st.warning('Click on the required dimension to select')
 
         # Output the final dimension list and convert to DataFrame
         if st.session_state.dimension:
-            #st.write('Selected dimension pairs: ', st.session_state.dimension)
+            # st.write('Selected dimension pairs: ', st.session_state.dimension)
             df = pd.DataFrame(st.session_state.dimension, columns=['Width', 'Height'])
-            #st.write('DataFrame of Selected Dimensions:')
-            st.data_editor(df)
+            # st.write('DataFrame of Selected Dimensions:')
+            st.data_editor(df, num_rows="dynamic")
 
 except Exception as e:
     print(e)
 
-    st.warning('Please select extract in the main page to detect the text, then come back here to select the required dimension text!')
+    st.warning(
+        'Please select extract in the main page to detect the text, then come back here to select the required dimension text!')
