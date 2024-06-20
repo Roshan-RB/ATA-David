59dd2362f75d1f3db96b2af3a5bb86c909c0563a
Roshan Rayala Bhaskar <62153742+Roshan-RB@users.noreply.github.com>
2024-06-20 10:25:01 +0200

    copy pasting old code

--- Image_Cropping.py
+++ Image_Cropping.py
@@ -16,11 +16,15 @@ import time
 import base64
 import io
 
+
+
+
 fa_css = '''
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <i class="fa-solid fa-bars";></i>
-'''
+''' 
 st.write(fa_css, unsafe_allow_html=True)
+
 
 reader = easyocr.Reader(['en'])
 
@@ -45,27 +49,25 @@ if 'pil_image' not in st.session_state:
 if 'rotatedimg' not in st.session_state:
     st.session_state.rotatedimg = None
 if 'tmp_file_path' not in st.session_state:
-    st.session_state.tmp_file_path = None
+ st.session_state.tmp_file_path = None
 if 'pdf_document' not in st.session_state:
     st.session_state.pdf_document  = None
 if 'bounds_rotated' not in st.session_state:
     st.session_state.bounds_rotated = None
 if 'image_with_boxes_rotated' not in st.session_state:
     st.session_state.image_with_boxes_rotated  = None
-if 'file_name' not in st.session_state:
-    st.session_state.file_name = None
+
 
 pdf_file = st.sidebar.file_uploader("Upload a PDF file", type="pdf")
 page_number = st.sidebar.number_input("Enter the page number:", min_value=1, format="%d", value=1)
+
 
 
 def preprocess(img):
      # Convert the image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-
     #st.image( gray, caption='grayscale_image', use_column_width='never')
-
     # Get the dimensions of the grayscale image
     height, width = gray.shape[:2]
     #st.write("Image Size (Width x Height):", width, "x", height)
@@ -73,7 +75,6 @@ def preprocess(img):
     new_width = width * 4
     new_height = height * 4
     #st.write("Image Size (Width x Height):", new_width, "x", new_height)
-
     # Resize the grayscale image
     resized_gray = cv2.resize(gray, (new_width, new_height))
     #thresholded_resized_image = cv2.adaptiveThreshold(resized_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
@@ -81,18 +82,8 @@ def preprocess(img):
     return resized_gray
 
 
-
 if pdf_file:
-    # Reset dimensions and file_name when a new file is uploaded
-    if 'pdf_file' in st.session_state and st.session_state.pdf_file != pdf_file:
-        st.session_state.dimension = []
     st.session_state.pdf_file = pdf_file
-    # Extract and display the file name without the extension
-    st.session_state.file_name = pdf_file.name.rsplit('.', 1)[0]
-    ZeichnungsNr = st.session_state.file_name
-    st.session_state.ZeichnungsNr = ZeichnungsNr
-    st.write(f"Zeichnungs- Nr.: {ZeichnungsNr}")
-
 
 if st.session_state.pdf_file is not None:
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
@@ -101,21 +92,18 @@ if st.session_state.pdf_file is not None:
     st.session_state.tmp_file_path = tmp_file_path
     try:
         pdf_document = fitz.open(st.session_state.tmp_file_path)
-        st.session_state.pdf_document = pdf_document
+        st.session_state.pdf_document  = pdf_document
 
         if page_number > len(st.session_state.pdf_document):
             st.error(f"Invalid page number. Maximum page number is {len(st.session_state.pdf_document)}.")
         else:
             st.session_state.page_number = page_number
             page = st.session_state.pdf_document.load_page(st.session_state.page_number - 1)
-
             image_bytes = page.get_pixmap().tobytes()
             st.session_state.image_bytes = image_bytes
 
             # Display the PDF page
-            st.markdown(
-                f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**PDF**</span>',
-                unsafe_allow_html=True)
+            st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**PDF**</span>', unsafe_allow_html=True)
             st.image(st.session_state.image_bytes, use_column_width=True, caption=f"Page {page_number}")
 
             # Use cropper to select and crop a part of the image only if the page number changes
@@ -124,10 +112,8 @@ if st.session_state.pdf_file is not None:
     except Exception as e:
         st.error(f"Error: {e}")
         st.session_state.pdf_file = None  # Clear the uploaded PDF file
-
     if st.session_state.crop_button_clicked:
         cropped_image = st_cropperjs(st.session_state.image_bytes, btn_text="Crop Image")
-
         if cropped_image:
             st.session_state.cropped_image = cropped_image
             with st.spinner("Loading the cropped image..."):
@@ -137,47 +123,53 @@ if st.session_state.pdf_file is not None:
                 # Convert cropped image to PIL Image
                 pil_image = Image.open(BytesIO(st.session_state.cropped_image))
                 st.session_state.pil_image = pil_image
-
                 # Display the cropped image
                 st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**Cropped Image**</span>', unsafe_allow_html=True)
                 #st.image(st.session_state.pil_image, use_column_width='auto')
                 width, height = st.session_state.pil_image.size
                 #st.write("Image Size (Width x Height):", width, "x", height)
 
-                 
+
+
                 # Save the cropped image as a PNG file
                 with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp_img_file:
                     st.session_state.pil_image.save(tmp_img_file.name)
-
                     # Read the cropped image back as bytes
                     with open(tmp_img_file.name, "rb") as img_file:
                         img_bytes = img_file.read()
 
+                    # Provide download button for the cropped image
+                    #st.download_button("Download Cropped Image", img_bytes, file_name="cropped_image.png", mime="image/png")
+
             except Exception as e:
                 st.error(f"Error: {e}")
 
-    # if st.session_state.pdf_file:
-    # try:
-    # st.session_state.pdf_document.close()  # Close the PDF document
-    # os.unlink(tmp_file_path)  # Clean up the temporary file
-    # except Exception as e:
-    # st.error(f"Error during cleanup: {e}")
+
+    #if st.session_state.pdf_file:
+        #try:
+            #st.session_state.pdf_document.close()  # Close the PDF document
+            #os.unlink(tmp_file_path)  # Clean up the temporary file
+        #except Exception as e:
+            #st.error(f"Error during cleanup: {e}")
 
 else:
     st.write("Upload a PDF file using the file uploader above.")
 
+
+
+
 if st.session_state.pil_image:
+
+
 
     def rotate_image(im, angle):
         return im.rotate(angle, expand=True)
 
-    
     image_np = np.array(st.session_state.pil_image)
 
     thresholded_resized_image = preprocess(image_np)
-   
-###code for non rotated image 
 
+###code for non rotated image 
 
     st.image(thresholded_resized_image)
 
@@ -185,18 +177,20 @@ if st.session_state.pil_image:
     Processed_Image = Image.fromarray(thresholded_resized_image)
     #st.image(Processed_Image)
     st.write('')
-    # st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**Processed Image**</span>', unsafe_allow_html=True)
-    # st.image(Processed_Image, use_column_width='auto')
+    #st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**Processed Image**</span>', unsafe_allow_html=True)
+    #st.image(Processed_Image, use_column_width='auto')
 
     n_width, n_height = Processed_Image.size
-    
+    #st.write("Image Size (Width x Height):", n_width, "x", n_height)
 
-    rotated_image = rotate_image(Processed_Image, -90)
-    st.write('')
-    # st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**Rotated Image**</span>', unsafe_allow_html=True)
-    # st.image(rotated_image, use_column_width='auto')
 
-  
+
+     # Convert PIL Image to JPEG format
+    img_byte_arr = BytesIO()
+    Processed_Image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+
 
     # Doing OCR. Get bounding boxes.
     bounds = reader.readtext(img_byte_arr,
@@ -207,12 +201,10 @@ if st.session_state.pil_image:
                             )
     #st.write(bounds)
     
-
     if bounds not in st.session_state:
         st.session_state.bounds = bounds
-    
-    def draw_boxes_horizontal(image, bounds, color1='red', color2='blue', width=4):
 
+    def draw_boxes_horizontal(image, bounds, color1='red', color2='blue', width=4):
         rgb_image = Image.new('RGB', image.size)
         rgb_image.paste(image)
         draw = ImageDraw.Draw(rgb_image)
@@ -242,7 +234,6 @@ if st.session_state.pil_image:
         rgb_image = Image.new('RGB', image.size)
         rgb_image.paste(image)
         draw = ImageDraw.Draw(rgb_image)
-
         if changed_dim is False:
             for bound in bounds:
                 p0, p1, p2, p3 = bound[0]
@@ -269,39 +260,24 @@ if st.session_state.pil_image:
                 draw.line([*p0, *p1, *p2, *p3, *p0], fill=fill_color, width=width)
             
             return rgb_image
-
-
-
-
-
-
     st.session_state.rotatedimg = rotate_image(st.session_state.pil_image, 270)
     image_np_rotated = np.array(st.session_state.rotatedimg)
-
     thresholded_resized_image_rotated = preprocess(image_np_rotated)
     #st.image(thresholded_resized_image_rotated)
     # Gaussian blur
     #blur = cv2.GaussianBlur(equ, (5, 5), 1)
-
-
     # Convert NumPy array back to PIL Image
     Processed_Image_rot = Image.fromarray(thresholded_resized_image_rotated)
     #st.image(Processed_Image_rot)
     st.write('')
     #st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**Processed Image**</span>', unsafe_allow_html=True)
     #st.image(Processed_Image_rot, use_column_width='auto')
-
     n_width_rot, n_height_rot = Processed_Image_rot.size
     #st.write("Image Size (Width x Height):", n_width, "x", n_height)
-
-
-
      # Convert PIL Image to JPEG format
     img_byte_arr_rot = BytesIO()
     Processed_Image_rot.save(img_byte_arr_rot, format='PNG')
     img_byte_arr_rot = img_byte_arr_rot.getvalue()
-
-
     # Doing OCR. Get bounding boxes.
     bounds_rotated = reader.readtext(img_byte_arr_rot,
                             allowlist="0123456789,",
@@ -309,15 +285,22 @@ if st.session_state.pil_image:
                             )
     #st.write(bounds)
     
-
     if bounds_rotated not in st.session_state:
         st.session_state.bounds_rotated = bounds_rotated
-    
+
+
+
+
+
+
+
+
+
+
+
 
     if st.button('Extract_text'):
-        st.markdown(
-            f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**OCR**</span>',
-            unsafe_allow_html=True)
+        st.markdown(f'<i class="fa-solid fa-cube" style="margin-right: 10px; font-size: 20px;"></i> <span style="font-size: 24px; color: #3573b3;">**OCR**</span>', unsafe_allow_html=True)
         with st.spinner('Extracting text...'):
             time.sleep(8)
 
@@ -325,14 +308,22 @@ if st.session_state.pil_image:
         #print(image_with_boxes.size)
         #st.image(image_with_boxes)
         st.session_state.image_with_boxes = image_with_boxes
-        
+
+
+        if 'image_with_boxes' in st.session_state:
+            st.image(st.session_state.image_with_boxes, use_column_width= 'auto')
+
+
+        #st.image(rotated_image_with_boxes, use_column_width= "auto"
 
 
 
+
+        #st.write('Extracted text:')
 
         ##correctly detects the vertical dimesnions
         image_with_boxes_rotated = draw_boxes_vertical(Processed_Image_rot.copy(), bounds_rotated)
-       
-        st.session_state.image_with_boxes_rotated = image_with_boxes_rotated
-     
+        #print(image_with_boxes_rotated.size)
+        #st.image(image_with_boxes_rotated)
 
+        st.session_state.image_with_boxes_rotated = image_with_boxes_rotated
