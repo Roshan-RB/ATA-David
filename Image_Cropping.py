cc1d4994778c0e4dda760cd9bcbb3b5d9adc6ec8
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-06-20 11:10:41 +0200

    Update Image_Cropping.py

--- Image_Cropping.py
+++ Image_Cropping.py
@@ -24,6 +24,8 @@ st.write(fa_css, unsafe_allow_html=True)
 
 reader = easyocr.Reader(['en'])
 
+#Testing 20.06
+
 st.title("Dimension Detection!")
 # Initialize session state variables
 if 'pdf_file' not in st.session_state:
