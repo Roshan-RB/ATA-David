56cc9145d04f735042b2a6d1d5ab6e55ba639f0a
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-07-31 12:18:19 +0200

    Testing

--- Sign_In.py
+++ Sign_In.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 #from menu import menu
 import warnings
 st.set_page_config(layout="wide")
@@ -16,5 +17,5 @@ if st.query_params.get_all == []:
     Home.main()  # Assuming you have a `main()` function in Home.py
 else:
     # Load the default login/register page
-    import login_register  # Assuming this script is saved as login_register.py
+    import login_register # Assuming this script is saved as login_register.py
     login_register.login_app()  # Assuming the login logic is in a function `login_app()`
